d2b4a186dedccc646d01d9c79a94c822aba431cd
Leanne Chen <lianchennnn@gmail.com>
2020-08-27 12:15:48 +0800

    Move Card style to its CSS module

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,30 +1,30 @@
 import React from 'react';
-import appStyle from "../App.module.css";
+import cardStyle from "./Card.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar, faCircle } from "@fortawesome/free-solid-svg-icons";
 
 function Card(props) {
   const { full_name, svn_url, description, stargazers_count, language, license } = props;
   return (
-    <li key={full_name} className={appStyle.card}>
-      <a href={svn_url} className={appStyle.cardLink} target="_blank" rel="noopener noreferrer">
+    <li key={full_name} className={cardStyle.card}>
+      <a href={svn_url} className={cardStyle.cardLink} target="_blank" rel="noopener noreferrer">
         <div>
-          <h2 className={appStyle.cardTitle}>{full_name}</h2>
-          <p className={appStyle.cardDesc}>{description}</p>
+          <h2 className={cardStyle.cardTitle}>{full_name}</h2>
+          <p className={cardStyle.cardDesc}>{description}</p>
         </div>
         <div>
-          <div className={appStyle.cardLabelContainer}>
-          <span className={appStyle.cardLabel}>
-            <FontAwesomeIcon icon={faStar} className={`${appStyle.cardIcon} ${appStyle.iconStar}`} />
+          <div className={cardStyle.cardLabelContainer}>
+          <span className={cardStyle.cardLabel}>
+            <FontAwesomeIcon icon={faStar} className={`${cardStyle.cardIcon} ${cardStyle.iconStar}`} />
             <span>{stargazers_count}</span>
           </span>
-            <span className={appStyle.cardLabel}>
-            <FontAwesomeIcon icon={faCircle} className={`${appStyle.cardIcon} ${appStyle.iconCircle}`} />
+            <span className={cardStyle.cardLabel}>
+            <FontAwesomeIcon icon={faCircle} className={`${cardStyle.cardIcon} ${cardStyle.iconCircle}`} />
             <span>{language}</span>
           </span>
           </div>
-          <div className={appStyle.cardFooter}>
-            <span className={appStyle.cardFootNote}>{license? license.name: "No license specified"}</span>
+          <div className={cardStyle.cardFooter}>
+            <span className={cardStyle.cardFootNote}>{license? license.name: "No license specified"}</span>
           </div>
         </div>
       </a>
